6d1a04a0ed35cf9f1fd3b4776282e8d0ddf3c1ba
Fari Nwa <farimomoh@gmail.com>
2023-02-08 23:10:43 +0000

    Task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,11 @@
-iii
+#!/usr/bin/env python3
+"""Task 0 Module"""
+
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) ->float:
+    """Waits for a random number in the range of max_delay"""
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
